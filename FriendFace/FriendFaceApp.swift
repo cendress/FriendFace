a954788a896f9aeaee0954a7ca1e059d8918871e
cendress <95492688+cendress@users.noreply.github.com>
2024-07-20 10:30:52 -0400

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Christopher Endress on 7/20/24.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
